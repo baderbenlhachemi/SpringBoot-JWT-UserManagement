729c4a7e4f4c46beba56e29975fbd28ea7778c73
baderbenlhachemi <baderbenlhachemi@gmail.com>
2024-03-25 16:03:25 +0000

    Changed package name.

---
+++ src/main/java/com/cirestechnologies/demo/model/Role.java
@@ -0,0 +1,29 @@
+package com.cirestechnologies.demo.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+
+    public Role() {
+    }
+
+    public Role(ERole name) {
+        this.name = name;
+    }
+
+}
--- src/main/java/ma/cirestechnologies/demo/models/ERole.java
+++
@@ -1,6 +0,0 @@
-package ma.cirestechnologies.demo.models;
-
-public enum ERole {
-    ROLE_USER,
-    ROLE_ADMIN
-}
